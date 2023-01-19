3d5d1914af3b3e1ca262453fe2589b4c3d4933ec
GhassenBoughzala <ghassen.boughzala@esprit.tn>
2023-01-19 16:44:13 +0100

    Update

--- src/app/doctors/doctors.component.ts
+++ src/app/doctors/doctors.component.ts
@@ -5,48 +5,44 @@ import { DoctorsService } from '../shared/doctors.service';
 @Component({
   selector: 'app-doctors',
   templateUrl: './doctors.component.html',
-  styleUrls: ['./doctors.component.css']
+  styleUrls: ['./doctors.component.css'],
 })
 export class DoctorsComponent implements OnInit {
-
   listdocs: Doctors[];
-  @Input() doctor:Doctors;
+  @Input() doctor: Doctors;
   @Output() notification = new EventEmitter<Doctors>();
-  searchinput:string;
+  searchinput: string;
   username: string;
-  cnt:number;
-  storeduser:Array<any>;
+  cnt: number;
+  storeduser: Array<any>;
 
-  constructor(private docService: DoctorsService,) { }
+  constructor(private docService: DoctorsService) {}
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.doctor.like = 0;
-    this.storeduser = JSON.parse(localStorage.getItem("connecteduser"));
-    if ((typeof this.storeduser !== 'undefined' && this.storeduser !== null)) {
-        this.cnt = 1;
+    this.storeduser = JSON.parse(localStorage.getItem('connecteduser'));
+    if (typeof this.storeduser !== 'undefined' && this.storeduser !== null) {
+      this.cnt = 1;
 
-        var test = localStorage.getItem('connecteduser');
-        interface us {
-        username:string;
+      var test = localStorage.getItem('connecteduser');
+      interface us {
+        username: string;
       }
       let obj: us = JSON.parse(test);
-      this.username=obj.username;
-
-      } else {
-        this.cnt = 0;
-      }
+      this.username = obj.username;
+    } else {
+      this.cnt = 0;
+    }
   }
 
-  sendNotif(){
-    
+  sendNotif() {
     this.notification.emit(this.doctor);
   }
 
-  search(){
-    this.listdocs=null;
-    this.docService.rechercherdocteur(this.searchinput.valueOf())
-                    .subscribe(
-                      (data: Doctors[]) => this.listdocs = data);
+  search() {
+    this.listdocs = null;
+    this.docService
+      .rechercherdocteur(this.searchinput.valueOf())
+      .subscribe((data: Doctors[]) => (this.listdocs = data));
   }
-
 }
